945ed1f3e3fc6b8ad7c06465441cf05af0209976
Nikhilesh <nikhileshmkgi30@gmail.com>
2025-04-10 15:53:46 +0530

    updated

---
+++ javaScript/Sped js/05function.js
@@ -0,0 +1,11 @@
+// function greet(name){
+//     console.log(`hellow${name}`);
+    
+// }
+// greet("nikhilesh")
+// greet("piyush")
+
+console.log(`my age is ${age}`);
+var age = 25
+console.log(`my age is ${age}`);
+
